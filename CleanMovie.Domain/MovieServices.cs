63a2c2137585651b6cffdaa2f2dbd6484ac4875e
Suman Rai <officeproj111@gmail.com>
2023-09-21 14:24:24 +0545

    Commit

---
+++ CleanMovie.Domain/MovieServices.cs
@@ -0,0 +1,34 @@
+﻿using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Application
+{
+    public class MovieServices : IMovieServices
+    {
+        //Constructor Dependency injection
+        private readonly IMovieRepository _movieRepository;
+     
+        public MovieServices(IMovieRepository movieRepository)
+        {
+
+            _movieRepository = movieRepository;
+
+        }
+
+        public Movie CreateMovie(Movie movie)
+        {
+            _movieRepository.CreateMovie(movie);
+            return movie;
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            var movies = _movieRepository.GetAllMovies();
+            return movies;
+        }
+    }
+}
